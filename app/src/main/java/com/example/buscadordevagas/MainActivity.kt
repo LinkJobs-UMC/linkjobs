33c7d590d8d0260a5761f538110021430c1add11
Teteu <contato@teteu.dev.br>
2023-10-17 20:19:39 -0300

    filter att

--- app/src/main/java/com/example/buscadordevagas/MainActivity.kt
+++ app/src/main/java/com/example/buscadordevagas/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.buscadordevagas
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
@@ -31,10 +32,23 @@ class MainActivity : AppCompatActivity() {
 
         editText = findViewById(R.id.input_buscador)
 
-        textViewTitulos = arrayOf(findViewById(R.id.titul_vaga1), findViewById(R.id.titul_vaga2), findViewById(R.id.titul_vaga3))
-        textViewEmpresas = arrayOf(findViewById(R.id.nome_empresa1), findViewById(R.id.nome_empresa2), findViewById(R.id.nome_empresa3))
-        textViewLocais = arrayOf(findViewById(R.id.local1), findViewById(R.id.local2), findViewById(R.id.local3))
-        imageViewArray = arrayOf(findViewById(R.id.logo_vaga1), findViewById(R.id.logo_vaga2), findViewById(R.id.logo_vaga3))
+        textViewTitulos = arrayOf(
+            findViewById(R.id.titul_vaga1),
+            findViewById(R.id.titul_vaga2),
+            findViewById(R.id.titul_vaga3)
+        )
+        textViewEmpresas = arrayOf(
+            findViewById(R.id.nome_empresa1),
+            findViewById(R.id.nome_empresa2),
+            findViewById(R.id.nome_empresa3)
+        )
+        textViewLocais =
+            arrayOf(findViewById(R.id.local1), findViewById(R.id.local2), findViewById(R.id.local3))
+        imageViewArray = arrayOf(
+            findViewById(R.id.logo_vaga1),
+            findViewById(R.id.logo_vaga2),
+            findViewById(R.id.logo_vaga3)
+        )
 
         populateData()
 
@@ -61,32 +75,63 @@ class MainActivity : AppCompatActivity() {
     private fun filterData(filterText: String) {
         if (filterText.isEmpty()) {
             populateData()
+            for (i in 0 until 3) {
+                showContainer(i)
+            }
         } else {
             var matchFound = false
             for (i in 0 until 3) {
-                if (titulos[i].contains(filterText, true) || empresas[i].contains(filterText, true) || locais[i].contains(filterText, true)) {
-                    imageViewArray.forEachIndexed { index, imageView ->
-                        if (i == index) {
-                            imageView.setImageResource(imagensArray[i])
-                        } else {
-                            imageView.setImageResource(0)
-                        }
-                    }
+                if (titulos[i].contains(filterText, true) || empresas[i].contains(
+                        filterText,
+                        true
+                    ) || locais[i].contains(filterText, true)
+                ) {
+                    imageViewArray[i].setImageResource(imagensArray[i])
                     textViewTitulos[i].text = titulos[i]
                     textViewEmpresas[i].text = empresas[i]
                     textViewLocais[i].text = locais[i]
                     matchFound = true
+                    showContainer(i) // Mostra o contêiner correspondente
                 } else {
                     imageViewArray[i].setImageResource(0)
                     textViewTitulos[i].text = ""
                     textViewEmpresas[i].text = ""
                     textViewLocais[i].text = ""
+                    hideContainer(i) // Oculta o contêiner sem correspondência
                 }
             }
             if (!matchFound) {
                 populateData()
+                for (i in 0 until 3) {
+                    showContainer(i)
+                }
             }
         }
     }
 
+    private fun hideContainer(index: Int) {
+        when (index) {
+            0 -> hideViewsInContainer(R.id.container_vaga1)
+            1 -> hideViewsInContainer(R.id.container_vaga2)
+            2 -> hideViewsInContainer(R.id.container_vaga3)
+        }
+    }
+
+    private fun showContainer(index: Int) {
+        when (index) {
+            0 -> showViewsInContainer(R.id.container_vaga1)
+            1 -> showViewsInContainer(R.id.container_vaga2)
+            2 -> showViewsInContainer(R.id.container_vaga3)
+        }
+    }
+
+    private fun hideViewsInContainer(containerId: Int) {
+        val container = findViewById<View>(containerId)
+        container.visibility = View.GONE
+    }
+
+    private fun showViewsInContainer(containerId: Int) {
+        val container = findViewById<View>(containerId)
+        container.visibility = View.VISIBLE
+    }
 }
